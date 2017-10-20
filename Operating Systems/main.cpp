dcca99a55dfe0bfe59cc87bf66ab065e62c1ca7a
Adrian <adrienkaminski@wp.pl>
2017-10-20 19:38:30 +0200

    3 Threads with proper library (pthread.h instead of thread from c11)

--- Operating Systems/main.cpp
+++ Operating Systems/main.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
-#include <thread>
-
+#include <pthread.h>
+#include <unistd.h>
 using namespace std;
 
+void* what_to_tell_a(void*)
+{
+	cout<<"Hello world, thread a there!\n";
+	//cout.flush();
+	void* result=NULL;
+	return result;
+}
+void* what_to_tell_b(void*)
+{
+	cout<<"Hello world, thread b there!\n";
+	//cout.flush();
+	void* result=NULL;
+	return result;
+}
+void* what_to_tell_c(void*)
+{
+	cout<<"Hello world, thread c there!\n";
+	//cout.flush();
+	void* result=NULL;
+	return result;
+}
 int main()
 {
-	cout<<"Hello world, no threads! Checking if *.o will be ignored"<<endl;
+	pthread_t a;
+	pthread_t b;
+	pthread_t c;
+	pthread_create(&a, NULL, &what_to_tell_a,NULL);
+	pthread_create(&b, NULL, &what_to_tell_b,NULL);
+	pthread_create(&c, NULL, &what_to_tell_c,NULL);
+	usleep(3);
+	cout<<"Hello world, main there!\n";
+	//cout.flush();
 
 	return 0;
 }
