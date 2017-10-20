15e596aa32a6057cd50ae766091dc84b9795a970
Adrian <adrienkaminski@wp.pl>
2017-10-20 20:12:35 +0200

    Threads, using while to wait for others. Still not sure if binary files are ignored.

--- Operating Systems/main.cpp
+++ Operating Systems/main.cpp
@@ -3,28 +3,30 @@
 #include <unistd.h>
 using namespace std;
 
+bool parent = true;
+int thread_child = 3;
 void* what_to_tell_a(void*)
 {
 	cout<<"Hello world, thread a there!\n";
-	//cout.flush();
 	void* result=NULL;
+	thread_child--;
 	return result;
 }
 void* what_to_tell_b(void*)
 {
 	cout<<"Hello world, thread b there!\n";
-	//cout.flush();
 	void* result=NULL;
+	thread_child--;
 	return result;
 }
 void* what_to_tell_c(void*)
 {
 	cout<<"Hello world, thread c there!\n";
-	//cout.flush();
 	void* result=NULL;
+	thread_child--;
 	return result;
 }
-int main()
+void* make_some_threads(void*)
 {
 	pthread_t a;
 	pthread_t b;
@@ -32,9 +34,20 @@ int main()
 	pthread_create(&a, NULL, &what_to_tell_a,NULL);
 	pthread_create(&b, NULL, &what_to_tell_b,NULL);
 	pthread_create(&c, NULL, &what_to_tell_c,NULL);
-	usleep(3);
+	while(thread_child>0)
+	{usleep(1);}
+	cout<<"Hello world, parent thread there!\n";
+	void* result=NULL;
+	parent = false;
+	return result;
+}
+int main()
+{
+	pthread_t parent_thread;
+	pthread_create(&parent_thread, NULL, &make_some_threads,NULL);
+	while(parent)
+	{usleep(1);}
 	cout<<"Hello world, main there!\n";
-	//cout.flush();
 
 	return 0;
 }
