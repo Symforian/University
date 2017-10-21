9d2ec65827dd31e2ac485574207a256b3e716772
Adrian <adrienkaminski@wp.pl>
2017-10-21 17:29:35 +0200

    Used pthread_join instead of usleep.

--- Operating Systems/P1/main.cpp
+++ Operating Systems/P1/main.cpp
@@ -1,52 +1,41 @@
 #include <iostream>
 #include <pthread.h>
-#include <unistd.h>
+
 using namespace std;
 
-bool parent = true;
-int thread_child = 3;
-void* what_to_tell_a(void*)
+void *what_to_tell_a(void*)
 {
 	cout<<"Hello world, thread a there!\n";
-	void* result=NULL;
-	thread_child--;
-	return result;
+	return NULL;
 }
-void* what_to_tell_b(void*)
+void *what_to_tell_b(void*)
 {
 	cout<<"Hello world, thread b there!\n";
-	void* result=NULL;
-	thread_child--;
-	return result;
+	return NULL;
 }
-void* what_to_tell_c(void*)
+void *what_to_tell_c(void*)
 {
 	cout<<"Hello world, thread c there!\n";
-	void* result=NULL;
-	thread_child--;
-	return result;
+	return NULL;
 }
-void* make_some_threads(void*)
+void *make_some_threads(void*)
 {
-	pthread_t a;
-	pthread_t b;
-	pthread_t c;
+	pthread_t a,b,c;
 	pthread_create(&a, NULL, &what_to_tell_a,NULL);
 	pthread_create(&b, NULL, &what_to_tell_b,NULL);
 	pthread_create(&c, NULL, &what_to_tell_c,NULL);
-	while(thread_child>0)
-	{usleep(1);}
+	pthread_join(a,NULL);
+	pthread_join(b,NULL);
+	pthread_join(c,NULL);
 	cout<<"Hello world, parent thread there!\n";
 	void* result=NULL;
-	parent = false;
 	return result;
 }
 int main()
 {
 	pthread_t parent_thread;
 	pthread_create(&parent_thread, NULL, &make_some_threads,NULL);
-	while(parent)
-	{usleep(1);}
+	pthread_join(parent_thread,NULL);
 	cout<<"Hello world, main there!\n";
 
 	return 0;
