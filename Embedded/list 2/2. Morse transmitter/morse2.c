b91ee6cb981bcecfa191d12eadb706004e2e77fc
Windux <windux@krogulec.home>
2019-10-15 22:50:12 +0200

    Task 2, list 2 Embedded. Clearing directories

--- Embedded/list 2/2. Morse transmitter/morse2.c
+++ Embedded/list 2/2. Morse transmitter/morse2.c
@@ -2,9 +2,19 @@
 #include <util/delay.h>                     /* Functions to waste time */
 #include <stdio.h>
 #include <inttypes.h>
+
+
 #define BAUD 9600                          // baudrate
-#define UBRR_VALUE ((F_CPU)/16/(BAUD)-1)   // zgodnie ze wzorem
-#define DEBOUNCE_TIME  1000                            /* microseconds */
+#define UBRR_VALUE ((F_CPU)/16/(BAUD)-1)   // zgodnie ze wzorem 
+                        /* microseconds */
+#define LED PB5
+#define LED_DDR DDRB
+#define LED_PORT PORTB
+
+#define BTN PD2
+#define BTN_PIN PIND
+#define BTN_PORT PORTD
+
 #define DOT 1000
 #define LINE 3000
 #define START 15
@@ -40,19 +50,20 @@ int uart_receive(FILE *stream)
 }
 
 FILE uart_file;
-uint8_t debounce(void) {
-    if (bit_is_clear(PIND, PD2)) {      /* button is pressed now */
-        _delay_us(DEBOUNCE_TIME);
-        if (bit_is_clear(PIND, PD2)) {            /* still pressed */
-            return (1);    
+
+uint8_t pressedHalfDot(void) {
+    if (bit_is_clear(BTN_PIN, BTN)){
+        _delay_ms(DOT/2);
+        if (bit_is_clear(BTN_PIN, BTN)){
+            return 1;    
         }  
     }
-    return (0);
+    return 0;
 }
 const char morse [30] = {'H','S','V','I','F','U','!','E','L','R','?','A','P','W','J','*','B','D','X','N','C','K','Y','T','Z','G','Q','M','@','O'};
 
 char morseChar(uint8_t x,uint8_t n,uint8_t i) {
-  //  printf("%d\r\n",n);
+    //printf("%d\r\n",n);
     if( x == 0) return morse[n];
     if( x&1 )
         return morseChar(x>>2,n-i,i>>1);
@@ -62,11 +73,6 @@ char morseCode(uint8_t x){
     return morseChar(x,START,ITER);
 }
 int main(void) {
-// to jak klik to . jak dl klik to _
-// T1 OUTPUT . _ ON SCREEN
-// znak spacja Z
-// word_space  S
-// kod spacja ' '
     // -------- Inits --------- //
 
   // zainicjalizuj UART
@@ -74,92 +80,70 @@ int main(void) {
   // skonfiguruj strumienie wejścia/wyjścia
   fdev_setup_stream(&uart_file, uart_transmit, uart_receive, _FDEV_SETUP_RW);
   stdin = stdout = stderr = &uart_file;
-    DDRB = 0xff; // led as output
-    DDRD &= ~(1 << PD2);    /* Data Direction Register B:
+    LED_DDR = 0xff; // led as output
+    DDRD &= ~(1 << BTN);    /* Data Direction Register D:
                                    enables input. */
-    PORTD |= (1<<PD2);  // pull up 
-    int8_t buffer = 0;
+    BTN_PORT |= (1<<BTN);  // pull up 
+    uint8_t buffer = 0;
+    int spaces = 0;
+    int spac = 1;
 
 //    int was_pressed = 0;
   // ------ Event loop ------ //
   while (1) {
-//areas of correction +- .5 s extract function <<<TODO 
-    if(bit_is_clear(PIND, PD2)){
-       _delay_ms(DOT);
-       if(bit_is_clear(PIND, PD2)){
-            _delay_ms(DOT);
-            if(bit_is_clear(PIND, PD2)){
-                    if(bit_is_clear(PIND, PD2)){
-                        PORTB = 0b00111100;
-                        _delay_ms(100);
-                        PORTB = 0b11000011;
-                        printf("_\r\n");
-                        buffer |= 2;
-                    }
-                    else
-                    {printf(".\r\n");
-                        buffer |= 1;}
-            }
-            else
-            {printf(".\r\n");
-                        buffer |= 1;}
-        }
-    }
-   /* else{
+    int dot =0;
+    if(pressedHalfDot()){
+        if(pressedHalfDot()){
+            if(pressedHalfDot()){
+                if(pressedHalfDot()){
+                    if(pressedHalfDot()){
+                        if(bit_is_clear(BTN_PIN, BTN)){ // after 2.5s
+                            LED_PORT |= _BV(LED);
+                            _delay_ms(500);
+                            LED_PORT &= ~_BV(LED);
+                            if(buffer >0 && spaces == 2){ buffer = 0; spaces = 0;}
+                            /*printf("_\r\n"); */buffer |= 2;spaces = 0;
+                        }// else dot=1;
+                    }// else dot = 1;
+                } else dot = 1;
+                           
+            } else  dot=1;
+        } else dot=1;
+    }//if not halfdot - ignore
+ if(dot){
+            if(buffer >0 && spaces == 2){ buffer = 0; spaces = 0;}
+            /*printf("!.\r\n"); */buffer |= 1; spaces = 0;
+ }
+ if(!bit_is_clear(BTN_PIN, BTN)){
     _delay_ms(DOT);
-    if(bit_is_clear(PIND, PD2)){
-        buffer<<=2;
- //       printf(" ");
-        } else {
-        _delay_ms(DOT);
-        _delay_ms(DOT);
-        if(bit_is_clear(PIND, PD2)){
-                printf("%c",morseCode(buffer));
-                buffer =0;
-                }
-        else {
-            _delay_ms(DOT);
-            _delay_ms(LINE);
-            if(!bit_is_clear(PIND, PD2)){
-                printf("%c\r\n",morseCode(buffer));
-                buffer =0;
-                }
-            }
+    if(buffer > 0 && !bit_is_clear(BTN_PIN, BTN)){
+        if(buffer > 0 && spaces == 3){     //buffer pelen
+             while((buffer & 0b11)==0){  // while there is no info on last buffer place
+                buffer>>=2;
+             }
+         //   printf("%d, %d<-\r\n",buffer,spaces);
+             printf("%1c",morseCode(buffer));
+             spac = 0;
+             buffer = 0;
+        //     if(spaces == 3){
+        //        printf(",");}
         }
-
-    }*/
-        if(!(buffer==0)) {
-                printf("%c\r\n",morseCode(buffer));
-                buffer =0;}
-    
-  }                                                  /* End event loop */
+        //else  // reset if 2 spaces between words
+        else{
+           // printf("%d  )\n\r",buffer);
+            if((buffer & 0b11000000)==0)
+                buffer<<=2;
+                LED_PORT |= _BV(LED);
+                _delay_ms(100);
+                LED_PORT &= ~_BV(LED);
+            spaces++;
+            }
+        } 
+        else if(!bit_is_clear(BTN_PIN, BTN) && (spaces < 7)){ spaces ++;}     
+        else if(!bit_is_clear(BTN_PIN, BTN) && !spac && (spaces == 7)) {printf(" "); spac = 1;spaces = 0;} 
+   } 
+                                                  
+ }                                      /* End event loop */
   return 0;                            /* This line is never reached */
 }
 
-
-
-/*
-4 znaki max, 3 mozliwosci . _ 0 
-00 = 0
-01 = . 
-10 = _
-7 3 2 1 0 i i i  0 0 i i  0 0 0 i
-E = 00 00 00 01
-Z = 01 01 10 10
-a b a= 0 b = 28
-R = . - .
-R = 00 01 10 01
-
-
-
-
-
-1 3 = 2
-1 7 = 4
-5 7 = 6
-
-
-
-
-
-*/
