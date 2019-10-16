ad9485e7b89e65158d1b7137e81cf6813766f335
Symforian <adrienkaminskig@gmail.com>
2019-10-16 14:48:16 +0200

    Clearing button's code - embedded

--- Embedded/list 2/1. Button/button.c
+++ Embedded/list 2/1. Button/button.c
@@ -6,10 +6,19 @@
 #define UBRR_VALUE ((F_CPU)/16/(BAUD)-1)   // zgodnie ze wzorem
 #define DEBOUNCE_TIME  1000                            /* microseconds */
 
+
+#define LED PB5
+#define LED_DDR DDRB
+#define LED_PORT PORTB
+
+#define BTN PB0
+#define BTN_PIN PINB
+#define BTN_PORT PORTB
+
 uint8_t debounce(void) {
-    if (bit_is_clear(PIND, PD2)) {      /* button is pressed now */
+    if (bit_is_clear(BTN_PIN, BTN)) {      /* button is pressed now */
         _delay_us(DEBOUNCE_TIME);
-        if (bit_is_clear(PIND, PD2)) {            /* still pressed */
+        if (bit_is_clear(BTN_PIN, BTN)) {            /* still pressed */
             return (1);    
         }  
     }
@@ -19,9 +28,9 @@ int main(void) {
 
     // -------- Inits --------- //
     DDRB = 0xff; // led as output
-    DDRD &= ~(1 << PD2);    /* Data Direction Register B:
+    DDRB &= ~(1 << BTN);    /* Data Direction Register B:
                                    enables input. */
-    PORTD |= (1<<PD2);  // pull up 
+    BTN_PORT |= (1<<BTN);  // pull up 
     int8_t buffor[2000];
     int counter = 0;
     int reading = 0;
@@ -29,11 +38,11 @@ int main(void) {
 //    int was_pressed = 0;
   // ------ Event loop ------ //
   while (1) {
-        if (!reading && bit_is_clear(PIND, PD2)){ reading ++;}
+        if (!reading && bit_is_clear(BTN_PIN, BTN)){ reading ++;}
       if(reading){
        // if(debounce()){
       //      if(! was_pressed){ 
-        if(bit_is_clear(PIND, PD2)){
+        if(bit_is_clear(BTN_PIN, BTN)){
                 buffor[counter] = 1;                        /* pressed */   
          //       was_pressed = 1; 
                 }
@@ -50,16 +59,16 @@ int main(void) {
     if(counter == 1000) { printing = 1;}
     else if(printing == 1 && counter == 0){ printing = 2; }
     if(printing == 1){
-            if(buffor[counter-1000]==0) { PORTB = 0b11000011;}
-            else if(buffor[counter-1000]==1)
-                 {PORTB = 0b00111100;}
+            if(buffor[counter-1000]==1) { LED_PORT |= _BV(LED);}
+            else if(buffor[counter-1000]==0)
+                 {LED_PORT &= ~_BV(LED);}
             }
 
     
     else if (printing == 2){
-            if(buffor[counter+1000]==0) { PORTB = 0b11000011;}
-            else if(buffor[counter+1000]==1)
-                 {PORTB = 0b00111100;}
+            if(buffor[counter+1000]==1) { LED_PORT |= _BV(LED);}
+            else if(buffor[counter+1000]==0)
+                 {LED_PORT &= ~_BV(LED);}
 
             }
 
